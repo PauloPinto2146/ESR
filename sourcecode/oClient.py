ca506359155fa532e4602e1b066dc5e1c350bb7d
Ricardo Silva <54157938+RSs22@users.noreply.github.com>
2024-11-25 18:52:26 +0000

    Update oClient.py

--- sourcecode/oClient.py
+++ sourcecode/oClient.py
@@ -2,68 +2,85 @@ import socket
 import threading
 import time
 import supportClient 
+import pickle
 
 class oClient:
     def __init__(self, server_address = supportClient.server, server_port = supportClient.port):
         self.server_address = server_address
         self.server_port = server_port
-        self.pops = []  # Lista de IP's de Points of Presence
+        self.pops = {}  # Nome do PoP: Lista de possiveis ligações
         self.best_pop = None
         self.timestamp = 5
+        self.max_retry = 5
+        self.retry_timeout = 15
+        self.threshold = 5
 
     def discover_pops(self):
         while True:
-            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   
-            print("Vou descobrir os Pops") 
-            message = "FIND_POPS"
-            clientSocket.sendto(message.encode(), (self.server_address, self.server_port))
-            
-            response, _ = clientSocket.recvfrom(1024)
-            response = response.decode('utf-8')
-            if "FOUND_POPS" in response:
-                listapops = response.split()[1:]
-                if listapops == []:
-                    self.pops = []
-                    print("TOU SEM POP")
-                self.pops = [(pop.split(":")[0], 12001) for pop in listapops]
-            print(self.pops)
-            clientSocket.close()
+            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            message = b"FIND_POPS"
+            response = self.send_message_with_ack(message, (self.server_address, self.server_port))
+            if response:
+                if response.startswith(b"FOUND_POPS "):
+                    # Separar a mensagem inicial da parte serializada
+                    pops_data = response[len(b"FOUND_POPS "):]
+                    
+                    # Desserializar os dados
+                    pops = pickle.loads(pops_data)
+                    self.pops = pops
             print(f"PoPs disponíveis: {self.pops}")
+            clientSocket.close()
             time.sleep(self.timestamp)
+
+    def send_message_with_ack(self, message,address):
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientSocket.settimeout(self.retry_timeout)
+        retries = 0
+        while retries < self.max_retry:
+            try:
+                print("Vou enviar mensagem para -> ",address)
+                clientSocket.sendto(message, address)
+                response, _ = clientSocket.recvfrom(1024)
+                return response
+            except socket.timeout:
+                retries += 1
+                print(f"Timeout ao enviar mensagem para {address}. Tentativa {retries}/{self.max_retry}")
+            except Exception as e:
+                print(f"Erro ao enviar mensagem para {address}: {e}")
+                retries += 1
+        clientSocket.close()
+        print(f"Não foi possível enviar mensagem para {address}")
+        return None
+
 
     def monitoring(self):
         while True:
             best_latency = float('inf')
             current_best_pop = None
-            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            print("Os meus pops são: ",self.pops)
-            if self.pops == []:
+            if self.pops == {}:
                 self.best_pop = None
                 print("Nenhum PoP disponível.")
             else:
                 for pop in self.pops:
-                    try:
-                        # Medir latência para cada PoP
-                        time_send = time.time()
-                        message = "CHECK_LATENCY"
-                        print(f"Vou Enviar Mensagem para {pop}")
-                        clientSocket.sendto(message.encode(), (pop[0],12001))
+                    for ipspos in self.pops[pop]:
+                        try:
+                            # Medir latência para cada PoP
+                            time_send = time.time()
+                            message = b"CHECK_LATENCY"
+                            receive_message = self.send_message_with_ack(message, (ipspos,12001))
 
-                        receive_message, _ = clientSocket.recvfrom(1024)
-                        receive_message = receive_message.decode('utf-8')
-
-                        # Calcula a latência
-                        if "LATENCYCHECK" in receive_message:  # Verifique a resposta correta
-                            latency = time.time() - time_send
-                            if latency < best_latency:
-                                best_latency = latency
-                                current_best_pop = pop
-
-                    except socket.timeout:
-                        print(f"Timeout ao verificar latência para o PoP {pop}")
-                        continue
-                    except Exception as e:
-                        print(f"Erro ao comunicar com o PoP {pop}: {e}")
+                            if receive_message:
+                                if receive_message.startswith(b"LATENCYCHECK"):
+                                    latency = time.time() - time_send
+                                    print(f"EU SOU {pop} e a minha latencia é -> {latency}")
+                                    if latency < best_latency: #and best_latency - latency > self.threshold:
+                                        best_latency = latency
+                                        current_best_pop = pop
+                        except socket.timeout:
+                            print(f"Timeout ao verificar latência para o PoP {pop}")
+                            continue
+                        except Exception as e:
+                            print(f"Erro ao comunicar com o PoP {pop}: {e}")
 
                 # Atualiza o melhor PoP
                 self.best_pop = current_best_pop
